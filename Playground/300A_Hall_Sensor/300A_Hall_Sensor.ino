cfa5996b5586b45ccc95a9f6d4c8526c1ba6d94a
jrh4567 <jacksonholbrook@hey.com>
2022-09-29 22:40:44 -0400

    Fixed conversion math; added Vref reading
    Fixed the math to accurately report the current, and added code to read the sensor Vref for greater accuracy.

--- Playground/300A_Hall_Sensor/300A_Hall_Sensor.ino
+++ Playground/300A_Hall_Sensor/300A_Hall_Sensor.ino
@@ -2,31 +2,35 @@
  * https://www.mouser.com/ProductDetail/ICE-Components/ISB-300-A-802?qs=6RXCB%2F3ptqS95L0cyUF1vw%3D%3D
  * Wire 1 (Red) - Vdd (Supply)
  * Wire 2 (Black) - Output
- * Wire 3 (Blue) - Ref. Output
+ * Wire 3 (Blue) - Ref. Voltage Output
  * Wire 4 (White) - Vss (Ground)
+ *  
+ * This code must be run on an arduino uno, or another board that can analogRead up to 5V.
  *  
  * Jackson Holbrook, HyTech Racing, 9/21/2022
  */
 
 #define sensorPin A0 //should be connected to sensor Pin 2 (black wire)
-int rawSensorValue; //Raw sensor reading
+#define refPin A1 //should be connected to sensor Pin 3 (blue wire)
+int sensorValue; //Raw sensor reading
 double rawVoltage; //Raw sensor voltage (in Volts)
 double sensorCurrent; //Current (in Amps) read from the sensor
 
 void setup() {
   Serial.begin(9600);
-  rawSensorValue = 0;
+  sensorValue = 0;
   rawVoltage = 0.0;
   sensorCurrent = 0.0;
 }
 
 void loop() {
-  rawSensorValue = analogRead(sensorPin);
-  rawVoltage = rawSensorValue * (5.0 / 1023.0); //formula from arduino.cc
-  sensorCurrent = (rawVoltage-2.5) / 100 * 6.667; //conversion to Amps
-  Serial.print("RSV: ");
-  Serial.print(rawSensorValue);
-  Serial.print("\tRaw Voltage: " + String(rawVoltage));
-  Serial.println("\tCurrent (A): " + String(sensorCurrent));
+  sensorValue = analogRead(sensorPin) - analogRead(refPin);
+  rawVoltage = sensorValue * (5.0 / 1023.0); //convert to Volts
+  sensorCurrent = rawVoltage / .006667; //convert to Amps
+//  Serial.printf("RSV: %4d\tRV: %4.4f\tAmps: %4.4f\n", analogRead(sensorPin), rawVoltage, sensorCurrent);
+  Serial.print("Raw: " + String(analogRead(sensorPin)));
+  Serial.print("\tV: " + String(rawVoltage));
+  Serial.print(" \tA: " + String(sensorCurrent));
+  Serial.println("");
   delay(10);
 }
